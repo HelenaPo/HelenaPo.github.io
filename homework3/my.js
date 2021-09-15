d444cdd71013e1cdcf574c373bd4da005af7a6eb
AlenaPodgornaja <alena.podgornaja@gmail.com>
2021-09-15 20:12:24 +0300

    new_homework3

--- homework3/my.js
+++ homework3/my.js
@@ -2,5 +2,5 @@
 (name !==undefined) && (age>18);
 (role === "Admin") && (occupation ==="CEO");
 occupation||role;
-(role === "Admin")||(name !== undefined && (age>18) );
+(role === "Admin")||(name !== undefined && (age>18);
 (role === "Admin") && (name !==undefined) && age;
