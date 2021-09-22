7d0878194aa2e8e68a491297a11448d4aac65f02
AlenaPodgornaja <alena.podgornaja@gmail.com>
2021-09-22 20:35:29 +0300

    new_homework7

--- homework7/my.js
+++ homework7/my.js
@@ -1,19 +1,19 @@
 "use strict";
-let n = prompt ("Введите порядковый номер числа Фибоначчи:")
+let n = prompt("Введите порядковый номер числа Фибоначчи:");
 let result = n => {
 let fib1 = 1;
 let fib2 = 1;
-for (let i = 2; i <= n; i++) {
+for(let i = 2; i <= n; i++) {
     let fib3 = fib1 + fib2;
     fib1 = fib2;
     fib2 = fib3;
     }
     return fib1;
 }
-alert ( result(n) );
+alert(result(n));
 
 
-let anyNomber = prompt("Введите любое число:")
+let anyNomber = prompt("Введите любое число:");
 
 nextNomber:
 for (let i = 2; i < anyNomber; i++) {
@@ -22,5 +22,5 @@ for (let i = 2; i < anyNomber; i++) {
       if (i % j === 0) continue nextNomber;
     }
   
-    alert( i );
+    alert(i);
   }
