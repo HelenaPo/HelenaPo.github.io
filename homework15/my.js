24e01516f299ce099bf6e6ba1dfa363cfb6dc9d5
AlenaPodgornaja <alena.podgornaja@gmail.com>
2021-10-12 18:59:03 +0300

    new_homework15

---
+++ homework15/my.js
@@ -0,0 +1,27 @@
+"use strict";
+const arr = [
+    {name: 'Vasya', surname: 'Ivanov'},
+    {name: 'Vanya', surname: 'Ivanov'},
+    {name: 'Albert', surname: 'Vasyliev'},
+  ]
+
+  const keyBy = arr.reduce((keyBy, arr) => {
+    if (!keyBy[arr.surname]){
+      keyBy[arr.surname]=[]
+    }
+    keyBy[arr.surname].push(arr)
+
+    return keyBy;
+}, {})
+    console.log(keyBy)
+
+  const keyBy = arr.reduce((keyBy, arr) => {
+      if (!keyBy[arr.name]){
+        keyBy[arr.name]=[]
+      }
+      keyBy[arr.name].push(arr);
+
+      return keyBy;
+  }, {})
+  console.log(keyBy)
+  
