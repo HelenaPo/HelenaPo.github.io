39d5b41aaf8645c3f4cbf349725bb5cf3841c2cb
AlenaPodgornaja <alena.podgornaja@gmail.com>
2021-10-12 19:04:17 +0300

    new_homework15

--- homework15/my.js
+++ homework15/my.js
@@ -3,25 +3,25 @@ const arr = [
     {name: 'Vasya', surname: 'Ivanov'},
     {name: 'Vanya', surname: 'Ivanov'},
     {name: 'Albert', surname: 'Vasyliev'},
-  ]
+  ];
 
   const keyBy = arr.reduce((keyBy, arr) => {
-    if (!keyBy[arr.surname]){
-      keyBy[arr.surname]=[]
+    if (!keyBy[arr.surname]) {
+      keyBy[arr.surname] = []
     }
     keyBy[arr.surname].push(arr)
 
     return keyBy;
-}, {})
-    console.log(keyBy)
+}, {});
+    console.log(keyBy);
 
   const keyBy = arr.reduce((keyBy, arr) => {
-      if (!keyBy[arr.name]){
-        keyBy[arr.name]=[]
+      if (!keyBy[arr.name]) {
+        keyBy[arr.name] = []
       }
       keyBy[arr.name].push(arr);
 
       return keyBy;
-  }, {})
-  console.log(keyBy)
+  }, {});
+  console.log(keyBy);
   
