23bf1ee331809e0de55c8adf300f3c9a380fb74c
AlenaPodgornaja <alena.podgornaja@gmail.com>
2021-10-13 19:22:10 +0300

    homework15

--- homework15/my.js
+++ homework15/my.js
@@ -1,27 +1,20 @@
 "use strict";
 const arr = [
-    {name: 'Vasya', surname: 'Ivanov'},
-    {name: 'Vanya', surname: 'Ivanov'},
-    {name: 'Albert', surname: 'Vasyliev'},
-  ];
+  { name: 'Vasya', surname: 'Ivanov' },
+  { name: 'Vanya', surname: 'Ivanov' },
+  { name: 'Albert', surname: 'Vasyliev' }
+];
+function keyBy(array, key) {
+  return array.reduce((objBy,user) => {
+      if ( !objBy[user[key]] ) {
+        objBy[user[key]] = [];
+       }
+      objBy[user[key]].push(user);
+      return objBy;
+      }, {}
+    )
+};
 
-  const keyBy = arr.reduce((keyBy, arr) => {
-    if (!keyBy[arr.surname]) {
-      keyBy[arr.surname] = []
-    }
-    keyBy[arr.surname].push(arr)
-
-    return keyBy;
-}, {});
-    console.log(keyBy);
-
-  const keyBy = arr.reduce((keyBy, arr) => {
-      if (!keyBy[arr.name]) {
-        keyBy[arr.name] = []
-      }
-      keyBy[arr.name].push(arr);
-
-      return keyBy;
-  }, {});
-  console.log(keyBy);
+console.log(keyBy(arr, 'name'));
+console.log(keyBy(arr, 'surname'));
   
