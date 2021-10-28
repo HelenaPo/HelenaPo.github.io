ccc877294c35702e7b8e4f5e6925486478dd37f3
AlenaPodgornaja <alena.podgornaja@gmail.com>
2021-10-28 19:52:56 +0300

    new_homework17

--- homework17/my.js
+++ homework17/my.js
@@ -1,26 +1,24 @@
 "use strict";
 
-//const select = document.querySelector('#container');
+const select = document.querySelector('#container');
 
-const select = document.getElementById("container")
-console.log(select)
+const select = document.getElementById("container");
 
-const elements = document.querySelectorAll("ol .third")
-console.log(elements)
+const elements = document.querySelectorAll("ol .third");
 
-const header = document.querySelector(".header")
+const header = document.querySelector(".header");
 header.innerHTML = ("Hello");
 
-const footer = document.querySelector(".footer")
-footer.classList.add("main")
-footer.classList.remove("main")
+const footer = document.querySelector(".footer");
+footer.classList.add("main");
+footer.classList.remove("main");
 
 const fourLi = document.createElement('li');
-fourLi.innerHTML = ("four")
-const ul = document.querySelector("ul")
-ul.append(fourLi)
+fourLi.innerHTML = ("four");
+const ul = document.querySelector("ul");
+ul.append(fourLi);
 
-const greenLi = document.querySelector("ol")
-greenLi.style.background = "green"
+const greenLi = document.querySelector("ol");
+greenLi.style.background = "green";
 
-footer.remove()
+footer.remove();
