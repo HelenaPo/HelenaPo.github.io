0391d7e189ae66f3616b624d1d7ffd531c8d6acc
AlenaPodgornaja <alena.podgornaja@gmail.com>
2021-10-28 19:46:25 +0300

    new_homework17

---
+++ homework17/my.js
@@ -0,0 +1,26 @@
+"use strict";
+
+//const select = document.querySelector('#container');
+
+const select = document.getElementById("container")
+console.log(select)
+
+const elements = document.querySelectorAll("ol .third")
+console.log(elements)
+
+const header = document.querySelector(".header")
+header.innerHTML = ("Hello");
+
+const footer = document.querySelector(".footer")
+footer.classList.add("main")
+footer.classList.remove("main")
+
+const fourLi = document.createElement('li');
+fourLi.innerHTML = ("four")
+const ul = document.querySelector("ul")
+ul.append(fourLi)
+
+const greenLi = document.querySelector("ol")
+greenLi.style.background = "green"
+
+footer.remove()
