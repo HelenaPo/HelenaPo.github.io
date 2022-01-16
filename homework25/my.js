4ebc69e12415df17a832b69ec5a996171e55a15a
AlenaPodgornaja <alena.podgornaja@gmail.com>
2022-01-16 09:33:54 +0200

    new_homework25

---
+++ homework25/my.js
@@ -0,0 +1,22 @@
+"use strict"
+
+//№1
+
+String.prototype.reverse = function () {
+    return this.split('')
+    .reverse()
+    .join('');
+}
+
+//№2
+const newArray = Array.prototype.sort;
+
+Array.prototype.sort = function (...args) {
+    return newArray.apply([...this, args]);
+}
+
+//№3
+
+NodeList.prototype.map = Array.prototype.map;
+NodeList.prototype.reduse = Array.prototype.reduce;
+NodeList.prototype.find = Array.prototype.find;
